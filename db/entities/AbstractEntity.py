bd365fa6231bd22ce4511cd19cd6cf3de9342b13
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-07-10 00:40:20 -0500

    ABC is no longer only "always be casting" :^)

--- db/entities/AbstractEntity.py
+++ db/entities/AbstractEntity.py
@@ -3,10 +3,12 @@ from sqlalchemy.schema import Column
 from sqlalchemy.sql import func
 from sqlalchemy.types import Integer, DateTime
 
+from abc import ABC
+
 Base = sqlalchemy.orm.declarative_base()
 
 
-class AbstractEntity(Base):
+class AbstractEntity(Base, ABC):
     __id = Column("id", Integer, primary_key=True)
     __created_at = Column("created_at", DateTime(timezone=True), server_default=func.now())
     __updated_at = Column("updated_at", DateTime(timezone=True), onupdate=func.now())
