a16a3c40f18987f16751b1750d7a3d220e577f0f
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-07-10 02:14:12 -0500

    Hmm fix for "base" entity to avoid sqlalchemy from getting confuzzled

--- db/entities/AbstractEntity.py
+++ db/entities/AbstractEntity.py
@@ -1,14 +1,15 @@
 import sqlalchemy.orm
+from sqlalchemy.orm import declared_attr
 from sqlalchemy.schema import Column
 from sqlalchemy.sql import func
 from sqlalchemy.types import Integer, DateTime
 
-from abc import ABC
 
-Base = sqlalchemy.orm.declarative_base()
+class AbstractEntity():
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
-
-class AbstractEntity(Base, ABC):
-    __id = Column("id", Integer, primary_key=True)
-    __created_at = Column("created_at", DateTime(timezone=True), server_default=func.now())
-    __updated_at = Column("updated_at", DateTime(timezone=True), onupdate=func.now())
+    id = Column("id", Integer, primary_key=True)
+    created_at = Column("created_at", DateTime(timezone=True), server_default=func.now())
+    updated_at = Column("updated_at", DateTime(timezone=True), onupdate=func.now())
--- db/entities/Player.py
+++ db/entities/Player.py
@@ -1,12 +1,14 @@
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
 
 from sqlalchemy.types import Integer, String
 
 from db.entities.AbstractEntity import AbstractEntity
 
+Base = declarative_base(cls=AbstractEntity)
 
-class Player(AbstractEntity):
-    __tablename__ = "player"
 
-    __battle_tag = Column("battle_tag", String(20), nullable=False)
-    __current_mmr = Column("current_mmr", Integer, nullable=False)
+class Player(Base):
+
+    battle_tag = Column("battle_tag", String(20), nullable=False)
+    current_mmr = Column("current_mmr", Integer, nullable=False)
