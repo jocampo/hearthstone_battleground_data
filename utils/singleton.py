ec5bd8c467a20ea62bf2aed984ff536885010a85
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-07-09 23:54:45 -0500

    Introduces the singleton metaclass

---
+++ utils/singleton.py
@@ -0,0 +1,15 @@
+class Singleton(type):
+    """
+    Utility class to be used as metaclass for singleton classes
+    """
+    def __init__(cls, name, bases, dic):
+        cls.__single_instance = None
+        super().__init__(name, bases, dic)
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__single_instance:
+            return cls.__single_instance
+        single_obj = cls.__new__(cls)
+        single_obj.__init__(*args, **kwargs)
+        cls.__single_instance = single_obj
+        return single_obj
