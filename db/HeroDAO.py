732adc1fe62e9937bcaa0bfc52653b67e3ca7e1b
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-07-10 20:37:12 -0500

    Placeholder MatchDAO and new method for HeroDAO

--- db/HeroDAO.py
+++ db/HeroDAO.py
@@ -12,5 +12,13 @@ class HeroDAO(AbstractDAO):
                 .one())
 
     @staticmethod
+    def get_by_name(hero_name: str) -> Hero:
+        return (HeroDAO.get_connection()
+                .query(Hero)
+                .filter(Hero.name == hero_name)
+                .one())
+
+
+    @staticmethod
     def list() -> list[Hero]:
         return [x for x in HeroDAO.get_connection().query(Hero).all()]
---
+++ db/MatchDAO.py
@@ -0,0 +1,5 @@
+from db.AbstractDAO import AbstractDAO
+
+
+class MatchDAO(AbstractDAO):
+    pass
